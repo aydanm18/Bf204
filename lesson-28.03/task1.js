9f9dd4a7c0b266dcb22fcacb6c897b14708bd339
Aydan <babayevaaydan18@gmail.com>
2024-03-28 19:44:46 +0400

    28.03

---
+++ lesson-28.03/task1.js
@@ -0,0 +1,206 @@
+
+// let age=+window.prompt("Enter your age")
+// if( age<125 && age>0)
+// {
+// if (age>65)
+// {
+// console.log("Uygundur");
+// }
+// else
+// {
+//     console.log("Uygun deyil");
+// }
+// }
+
+// let number= +window.prompt("Enter your number");
+
+// if(number&2==0 && number>0){
+// console.log("Eded cutdur");
+// }
+// else
+// {
+//     console.log("Eded tekdir");
+// }
+
+// let month = String.prototype.toLowerCase(window.prompt("Enter your month"));
+// switch (month) {
+//     case 0:
+//     case 1:
+//     case 2:
+//         console.log("Qis");
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log("Yaz");
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log("Yay");
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log("Payiz");
+//         break;
+
+// }
+
+// let number = +window.prompt("Enter your number");
+// if (number>0) {
+// if(number%3==0 && number%5==0)
+// {
+//     console.log("Eded dogrudur")
+// }
+// else{
+//     console.log("Eded yanlisdir")
+// }
+// }
+
+//  let number = +window.prompt("Enter your number");
+//  if (number>0) {
+//  if(number%2==0)
+//  {
+//      console.log("Eded bolundu")
+//  }
+//  else{
+//     console.log(number%2)
+    
+//  }
+//  }
+// else{
+//     console.log("Qaliq hesablandi");
+// }
+
+// let number1=+window.prompt("Eded daxil et");
+// let number2=+window.prompt("Eded daxil et");
+// let vur=number1 * number2
+// if(vur>0){
+//     console.log("Eded musbetdir");
+// }
+
+// else{
+//     console.log("Eded menfidir");
+// }
+
+// let number = +window.prompt("Enter your number");
+// if(number>0)
+// {
+// if(number>=1 && number<=100){
+//     console.log("Eded dogrudur");
+// }
+// else
+// {
+//     console.log("Eded yanlisdir");
+// }
+// }
+
+// let number1=+window.prompt("Eded daxil et");
+//  let number2=+window.prompt("Eded daxil et");
+//  let number3=+window.prompt("Eded daxil et");
+//   if(number1>number2 && number1>number3){
+//     console.log(" Number1 Max");
+//   }
+//   else if (number2>number1 && number2>number3){
+//    console.log("Number2 Max");
+//   }
+//   else{
+//     console.log("Number3 Max");
+//   }
+
+// let number = +window.prompt("Enter your number");
+// if(number<51 )
+// {
+// console.log("F");
+// }
+// else if(number>50 && number<=60){
+//     console.log("E");
+// }
+// else if(number>60 && number<=70){
+//     console.log("D");
+// }
+// else if(number>70 && number<=80){
+//     console.log("C");
+// }
+// else if(number>80 && number<=90){
+//     console.log("B");
+// }
+// else if(number>90 && number<=100){
+//     console.log("A");
+// }
+
+
+// for(let i=1 ; i<100 ; i++)
+// {
+//     if(i % 2==0){
+//         console.log(" " + i );
+//     }
+// }
+
+// let number=+window.prompt("Enter your number :");
+// for (let i = 0; i <= number; i++) {
+//    if( number%i==0){
+//     console.log(i);
+//    }
+    
+// }
+
+// let number1=+window.prompt(" 1ci Eded daxil et");
+// let number2=+window.prompt(" 2 ci Eded daxil et");
+// let number3=+window.prompt(" 3 ci Eded daxil et");
+// a=number1**2;
+// b=number2**2;
+// c=number3**2;
+
+// if( a+b==c || a+c==b || b+c==a)
+// {
+// console.log("Duzbucaqli ucbucatdir");
+// }
+// else{
+//     console.log("Deyil");
+// }
+
+// let a=Number(window.prompt("Enter your number1"))
+// let b=Number(window.prompt("Enter your number2"))
+// let c=Number(window.prompt("Enter your number3"))
+// if(a==b && b==c ){
+// console.log("Berabertereflidir")
+// }
+// else{
+//     console.log("Beraberterefli deyil")
+// }
+
+// let a=Number(window.prompt("Enter your number1"))
+// let b=Number(window.prompt("Enter your number2"))
+// let c=Number(window.prompt("Enter your number3"))
+// if(a==b || a==c || c==b){
+
+//     console.log("Beraberyanlidir")
+//      }
+    
+//      else{
+//          console.log("Beraberyanli  deyil")
+//      }
+
+                             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
